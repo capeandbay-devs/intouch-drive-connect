1bfc036e8fadcec9057c9cdf3e79652c00e858f9
Angel Gonzalez <angel@capeandbay.com>
2020-02-21 18:10:14 -0500

    Appointments Obj
    Yup.

---
+++ src/app/Library/Appointments/Appointment.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace CapeAndBay\InTouch\Library\Appointments;
+
+use CapeAndBay\InTouch\Library\Feature;
+
+class Appointment extends Feature
+{
+    protected $url = '/appointments';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function appointments_url()
+    {
+        return $this->intouch_client->public_url().$this->url;
+    }
+
+    public function postAppointment($club_uuid, $data = [])
+    {
+        $results = false;
+
+        try
+        {
+            $args = [
+                'owner' => $data['owner'],
+                'lead' => $data['lead'],
+                'startDate' => $data['startDate'],
+                'endDate' => $data['endDate'],
+                'type' => $data['type'],
+                //'description' => '',
+                //'attendees' => ''
+            ];
+
+            $header = ['clubuuid: '.$club_uuid];
+            $response = $this->intouch_client->post($this->appointments_url(), $args, $header);
+
+            if($response)
+            {
+                $results = $response;
+            }
+        }
+        catch(\Exception $e)
+        {
+            $results = $e->getMessage();
+        }
+
+        return $results;
+    }
+}
--- src/config/intouch.php
+++ src/config/intouch.php
@@ -7,6 +7,7 @@ return [
         'token' => env('INTOUCH_AUTH_TOKEN',''),
     ],
     'class_maps' => [
+        'appointments' => \CapeAndBay\InTouch\Library\Appointments\Appointment::class,
         'leads' => \CapeAndBay\InTouch\Library\Leads\Leads::class,
         'leadsources' => \CapeAndBay\InTouch\Library\Leads\LeadSources::class,
         'staff' => \CapeAndBay\InTouch\Library\People\Staff::class,
