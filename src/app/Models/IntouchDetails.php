8a633542bb4a29e52a8787de8b3edaedd364a1b6
Angel Gonzalez <angel@capeandbay.com>
2021-12-27 12:07:35 -0500

    IntouchDetails Model

---
+++ src/app/Models/IntouchDetails.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace CapeAndBay\InTouch\app\Models;
+
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class IntouchDetails extends Model
+{
+    use  SoftDeletes, Uuid;
+
+    protected $primaryKey  = 'id';
+
+    /**
+     * The "type" of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['client_uuid','club_id', 'detail_name', 'intouch_detail_uuid', 'detail_value', 'misc', 'active'];
+    //protected $guarded = [];
+
+    protected $casts = [
+        'misc' => 'array',
+    ];
+}
